1a61a64505fc91c95a4279ec6442fafb5db8c8d9
MICAELE LIMA <micaelexavierdelima@gmail.com>
2025-06-10 20:17:57 -0300

    Conceito de Array

---
+++ array.js
@@ -0,0 +1,46 @@
+//1. criar uma lista
+let nomes = ["Carlos", "Daniel", "MARIA", "PEDRO", "CARLA"];
+let carros = ["FUSCA", "GOL", "PALIO", "CORSA", "UNO"];
+
+let idades = [10, '20', 30, '40', ];
+
+const cidades = ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Curitiba', 'Salvador'];
+
+// length retorno a quantidade de elementos do Array(l,ista)
+//console.log(`tamanho: ${cidades.length}`);
+
+//2. limpar elementos
+//console.log(idades);
+//idades = []
+//console.log(idades);
+
+//3.adicionar elementos
+
+
+//push adiciona elementos no final do array
+//carros.push("BMW");
+//console.log(carros);
+
+//4. remover elementos
+//pop remove o último elemento do array
+//console.log(nomes);
+//nomes.pop();
+//console.log(nomes);
+//nomes.shift();
+//console.log(nomes);
+//nomes.shift();
+// /*console.log(nomes);
+// console.log(nomes[1]); //acessar o segundo elemento do array
+
+// console.log()
+// let nomeProprio = `${nomes[0]} ${nomes[1]}`
+// console.log(nomeProprio.toUpperCase()); */ 
+
+//5.buscar elementos
+
+//6.ordenar
+
+let frutas = ["maçã", "banana", "laranja", "uva", "abacaxi"];
+frutas.forEach(function (fruta, item) {
+    console.log(fruta, item)
+});
